1efda8714d9df8a0bc0efb281e9fd859774632f3
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-04-02 00:16:21 +0530

    Added partitions to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,14 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div className="h-screen w-full">
-        <div>
+      <div className="h-screen pt-8 w-screen flex justify-between flex-col bg-red-400 ">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/1200px-Uber_logo_2018.svg.png"
+          className="w-15 ml-8"
+        />
+        <div className="bg-white text-black py-5 px-10">
           <h2>Get Started with RideCalling</h2>
-          <button>Continue</button>
+          <button className="text-amber-300">Continue</button>
         </div>
       </div>
     </div>
