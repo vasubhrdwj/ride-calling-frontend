3166c08d6d3986474969f44a5c94166a2b565312
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-04-01 23:22:32 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Routes></Routes>
+    </div>
+  );
+};
+
+export default App;
